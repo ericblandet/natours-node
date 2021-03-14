0fb70bcc485d6689403dd5b9cb50f2d15533594d
Eric blandet <ericblandet@Erics-MacBook-Pro.local>
2021-03-14 17:21:34 +0100

    bcrypt installed

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,7 +25,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // it only works on SAVE and CREATE !!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'You misspelled your password',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // only runs the function if the password was modified
+
+  this.password = await bcrypt.hash(this.password, 12); // encrypts the password, with cost of 12
+
+  this.passwordConfirm = undefined; // we should not store the password confirmation !!
 });
 
 const User = mongoose.model('User', userSchema);
